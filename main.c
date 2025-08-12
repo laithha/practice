c50d31ac9a4314e7c88ae2fadc09f33c4017fc28
laithha <147397351+laithha@users.noreply.github.com>
2025-08-12 12:14:11 +0400

    practice...

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+Problem 1: Print numbers from 1 to n (input from user)
+
+Problem 2: Calculate and print the sum of n numbers (input one by one)
+
+Hints:
+
+Use a for loop for printing
+
+Use input() and int() conversion
+
+Accumulate sum in a variable
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("enter a number to print from 1 to your number:");
+    scanf(num);
+    for (int i > 0; i < num; i++){
+        printf(num);
+    }
+}
