09188317f6ccab8ee9ba47607c7d6b032651a9bd
laithha <147397351+laithha@users.noreply.github.com>
2025-08-13 13:36:16 +0400

    added some features

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 dinosaur = []
-def listDinosaurs ():
-    print(dinosaur)
-while True:
-    print("1.Add dinosaur")
-    print("2.List all dinos in the zoo")
-    print("3.exit")
-    choice = int(input("choose: "))
-    
-    if choice == 1:
+
+def deleteDino():
+    dinoid = int(input("which dino do you want to delete:"))
+    for d in dinosaur:
+        if d[0] == dinoid:
+            dinosaur.remove(d)
+        break
+
+def Adddino():
         dino_id = int(input("enter Id:"))
         species = int(input("enter species code:"))
         Age = int(input("enter the age: "))
         dinosaur.append([dino_id,species,Age])
+
+def listDinosaurs ():
+    print(dinosaur)
+
+while True:
+    print("1.Add dinosaur")
+    print("2.List all dinos in the zoo")
+    print("3.delete dino by id")
+    print("4.exit")
+    choice = int(input("choose: "))
+    if choice == 1:
+        Adddino()
     elif choice == 2:
         listDinosaurs()
     elif choice == 3:
+     deleteDino()
+    elif choice == 4:
         break
