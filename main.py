b7c5ef5acea088774d11d2a1da08c40795c61604
laithha <147397351+laithha@users.noreply.github.com>
2025-09-09 14:13:59 +0400

    done

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from collections import Counter
-from typing import List
+nums = [1, 3, 2, 3, 4, 1, 3]
 
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+def mostFrequent(nums: list[int]) -> int:
+    counts = {}
+    max_count = 0
+    most_freq = None
+    
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+        if counts[num] > max_count:
+            max_count = counts[num]
+            most_freq = num
+    
+    return most_freq
 
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.hasDuplicate([1, 2, 3, 1]))
+print(mostFrequent(nums)) 
